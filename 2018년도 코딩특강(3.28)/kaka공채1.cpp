ddffdcd675de538dd2a7d96d2bfad40caea1bafe
Park.Dong.Hyuck <SnowFleur0128@gmail.com>
2018-03-28 20:01:14 +0900

    18.03.28 commit

---
+++ 2018년도 코딩특강(3.28)/kaka공채1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+
+
+const int g_number = 5;
+
+
+int main() {
+	int array1[] = { 9,20,28,18,11 };
+	int array2[] = { 30,1,21,17,28 };
+	std::string result[16];
+
+	[](int a) {
+		a = 5;
+		std::cout << "´ä"<<a << std::endl;
+	
+	};
+
+
+
+}
+
+
+
+
+
