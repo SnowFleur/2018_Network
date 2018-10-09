beaa29aad238ba582d2762f43cd93944b4718953
SnowFleur <SnowFleur0128@gmail.com>
2018-10-10 08:42:14 +0900

    18.10.10

---
+++ 과제/과제6(clinet).cpp
@@ -0,0 +1,123 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32")
+#include<WinSock2.h>
+#include<iostream>
+#include<fstream>
+#define SERVERPORT 9000
+#define SERVERIP "127.0.0.1"
+#define BUFSIZE 1400
+
+
+using ifstrem = std::ifstream;
+using ofstrem = std::ofstream;
+
+
+struct FileInfor {
+	char name[255];
+	DWORD fileSize;
+
+};
+
+int main() {
+	char name[255];
+	FileInfor file;
+
+	std::cout << "파일이름을 입력하세요:";
+	std::cin >> file.name;
+
+	ifstrem uploadFile(file.name, std::ios::binary | std::ios::in);
+
+
+	if (uploadFile.fail())
+		std::cout << "파일을 열지 못했습니다." << std::endl;
+
+	//파일의 마지막으로 포인터 옮김
+	uploadFile.seekg(0, std::ios::end);
+	//파일사이즈를 구함
+	file.fileSize = uploadFile.tellg();
+	// 다시 파일의 처음으로 포인터를 옮김
+	uploadFile.seekg(0, std::ios::beg);
+
+
+
+	WSADATA wsa;
+	int retval;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		std::cout << "WSAStartUP error" << std::endl;
+		return 0;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		std::cout << "INVALID_SOCKET" << std::endl;
+		return 0;
+	}
+
+	//connet
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(SERVERPORT);
+	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	retval = connect(sock, reinterpret_cast<SOCKADDR*>(&serveraddr), sizeof(serveraddr));
+
+
+
+	char buf[BUFSIZE]; //한번에 보낼 양
+	if (retval == SOCKET_ERROR) {
+		std::cout << "connect error" << std::endl;
+		return 0;
+	}
+
+
+	/*
+	파일크기 고정전송
+	*/
+	retval = send(sock, reinterpret_cast<char*>(&file), sizeof(file), 0);
+	if (retval == SOCKET_ERROR) {
+		std::cout << "고정 오류" << std::endl;
+	}
+
+
+	std::cout << "보낸 파일이름:" << file.name << std::endl;
+	std::cout << "보낸 파일크기:" << file.fileSize << std::endl;
+
+
+	int count = file.fileSize / BUFSIZE;
+	int saveCount = count;
+
+	while (count) {
+
+		if (retval == 0)
+			break;
+
+		uploadFile.read(buf, BUFSIZE);
+		retval = send(sock, buf, BUFSIZE, 0);
+		if (retval == SOCKET_ERROR) {
+			std::cout << "가변 오류" << std::endl;
+			exit(1);
+		}
+	//	std::cout << "진행률"<<(saveCount-count)*100/saveCount << std::endl;
+		
+		--count;
+	}
+
+	//남은 크기 전송
+	 int remainData= file.fileSize-(saveCount*BUFSIZE) ;
+
+
+	uploadFile.read(buf, remainData);
+	retval = send(sock, buf, remainData, 0);
+	if (retval == SOCKET_ERROR) {
+		std::cout << "가변 오류" << std::endl;
+		exit(1);
+	}
+//	 std::cout << "진행률100%" << std::endl;
+
+	
+	closesocket(sock);
+	WSACleanup();
+
+	system("pause");
+
+}
---
+++ 과제/과제6(server).cpp
@@ -0,0 +1,241 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32")
+#include<iostream>
+#include<winsock2.h>
+#include<fstream>
+#include<vector>
+
+#define SERVERPORT 9000
+#define BUFSIZE 1400
+using ifstrem = std::ifstream;
+using ofstrem = std::ofstream;
+using vector = std::vector<HANDLE>;
+CRITICAL_SECTION cs;
+vector vThread;
+
+
+DWORD WINAPI Server(LPVOID);
+int recvn(SOCKET, char*, int, int);
+
+struct FileInfor {
+	char name[255];
+	DWORD fileSize;
+
+};
+
+HANDLE FileEvent;
+int FileCount = 0;
+int FileMaxCount = 0;
+
+
+int main() {
+
+	InitializeCriticalSection(&cs);
+
+
+	int retval;
+
+	FileEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+	//socket
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listen_sock == INVALID_SOCKET)
+		return 0;
+	//bind
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(SERVERPORT);
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	retval = bind(listen_sock, reinterpret_cast<SOCKADDR*>(&serveraddr), sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) {
+		std::cout << "bind error" << std::endl;
+		return 0;
+	}
+	//listen
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) {
+		std::cout << "listen error" << std::endl;
+		return 0;
+	}
+
+	//통신에 사용할 변수
+	SOCKET client_socket;
+	SOCKADDR_IN client_addr;
+	HANDLE hThread;
+	int addrlen;
+
+
+	while (1) {
+		//accept
+		addrlen = sizeof(client_addr);
+		client_socket = accept(listen_sock, reinterpret_cast<SOCKADDR*>(&client_addr), &addrlen);
+
+		if (client_socket == INVALID_SOCKET) {
+			std::cout << "accept error" << std::endl;
+			break;
+		}
+
+		hThread = CreateThread(NULL, 0, Server, reinterpret_cast<LPVOID>(client_socket), 0, NULL);
+		vThread.push_back(hThread);
+
+		FileMaxCount++;
+		std::cout << "[TCP서버] 클라이언트접속 IP주소: " << inet_ntoa(client_addr.sin_addr) << " 포트번호: " << ntohs(client_addr.sin_port) << std::endl;
+
+
+	}
+
+	//closesocket
+	closesocket(listen_sock);
+	WSACleanup();
+
+	system("pause");
+	return 0;
+}
+DWORD WINAPI Server(LPVOID arg) {
+
+	SOCKET client_socket = reinterpret_cast<SOCKET>(arg);
+	SOCKADDR_IN client_addr;
+	int retval;
+	int addrlen;
+	FileInfor file;
+	ofstrem filedownload;
+	char buf[BUFSIZE + 1];
+	int len = 0;
+	int fileCount;
+	unsigned int count;
+
+	addrlen = sizeof(client_addr);
+
+	getpeername(client_socket, reinterpret_cast<SOCKADDR*>(&client_addr), &addrlen);
+
+
+	while (1) {
+
+
+		//데이터 받기 (고정길이)
+		retval = recvn(client_socket, reinterpret_cast<char*>(&file), sizeof(file), 0);
+
+
+		if (retval == SOCKET_ERROR) {
+			std::cout << "recvn error" << std::endl;
+			break;
+		}
+		else if (retval == 0)
+			break;
+
+		std::cout << "받은 파일이름:" << file.name << std::endl;
+		std::cout << "받은 파일크기:" << file.fileSize << std::endl;
+
+		filedownload.open(file.name, std::ios::binary);
+
+
+		int count = file.fileSize / BUFSIZE;
+		int saveCount = count;
+
+
+
+
+		while (count) {
+
+			//데이터 받기 (가변길이)
+			retval = recvn(client_socket, buf,
+				BUFSIZE, 0);
+
+			if (retval == SOCKET_ERROR) {
+				std::cout << "recvn error" << std::endl;
+				break;
+			}
+			else if (retval == 0)
+				break;
+
+
+
+
+			EnterCriticalSection(&cs);
+			FileCount++;
+			if (FileCount == FileMaxCount) {
+				SetEvent(FileEvent);
+				FileCount = 0;
+			}
+			LeaveCriticalSection(&cs);
+			//retval = WaitForMultipleObjects(vThread.size(), vThread.data(), true, INFINITE);
+			retval = WaitForSingleObject(FileEvent, INFINITE);
+
+		
+
+			std::cout << "파일이름:" << file.name << "진행률" << (saveCount - count) * 100 / saveCount << "%" << std::endl;
+			ResetEvent(FileEvent);
+
+			system("cls");
+
+
+
+			
+
+			//EnterCriticalSection(&cs);
+			//FileCount++;
+			//std::cout<<"파일이름:"<<file.name<< "진행률" << (saveCount - count) * 100 / saveCount<<"%"<< std::endl;
+			//if (FileCount == FileMaxCount) {
+			//	system("cls");
+			//	FileCount = 0;
+			//}
+			//LeaveCriticalSection(&cs);
+
+
+			filedownload.write(buf, BUFSIZE);
+
+
+	
+			--count;
+		}
+
+
+		//남은길이
+		int remainData = file.fileSize - (saveCount*BUFSIZE);
+		retval = recvn(client_socket, buf, remainData, 0);
+		std::cout << "진행률100%" << std::endl;
+
+		EnterCriticalSection(&cs);
+		FileMaxCount--;
+		LeaveCriticalSection(&cs);
+
+
+		filedownload.write(buf, remainData);
+		filedownload.close();
+
+
+
+		std::cout << "TCP" << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << "]" << std::endl;
+	}
+
+	return 0;
+}
+
+
+
+int recvn(SOCKET s, char* buf, int len, int flag) {
+	int received;
+	char* ptr = buf;
+	int left = len;
+	static int i = 0;
+	while (left > 0) {
+		received = recv(s, ptr, left, flag);
+		if (received == SOCKET_ERROR)
+			return 0;
+
+		else if (received == 0)
+			break;
+
+		left -= received;
+		ptr += received;
+
+
+	}
+	return (len - left);
+}
+
+
